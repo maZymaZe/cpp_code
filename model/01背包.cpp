d90f841669f8fd837bfcc2e0f2ac074e627f2b8a
maze <782618517@qq.com>
2020-05-20 19:12:02 +0800

    01

--- model/01背包.cpp
+++ model/01背包.cpp
@@ -1,29 +1,20 @@
 #include<cstdio>
+#define MMAX 13000
+#define NMAX 3500
 int max(int a,int b){
-    if(a>b)return a;
-    else return b;
+    return a > b ? a : b;
 }
-struct wu{
-    int w,v;
-}a[400];
-int main()
-{   
-    int m,n,f[2000]={0};
-    scanf("%d%d",&m,&n);
-    for(int i=1;i<=n;i++)
-    {
-        scanf("%d%d",&a[i].w,&a[i].v);
+int c[NMAX],w[NMAX],dp[MMAX],n,v;
+int main(){
+    scanf("%d%d",&n,&v);
+    for(int i=0; i<n; i++){
+        scanf("%d%d",&c[i],&w[i]);
     }
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=m;j>=a[i].w;j--)
-        {
-            if(j>=a[i].w)
-            {
-                f[j]=max(f[j],f[j-a[i].w]+a[i].v);
-            }
+    for(int i=0; i<n; i++){
+        for(int j=v;j>=c[i];j--){
+            dp[j]=max(dp[j],dp[j-c[i]]+w[i]);
         }
     }
-    printf("%d",f[m]);
+    printf("%d",dp[v]);
     return 0;
 }
