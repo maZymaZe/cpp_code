1fca668c4d8ba246bb109fc3bfbd65ec621a8d97
maze <782618517@qq.com>
2020-06-20 09:39:48 +0800

    泛型函数测试

--- learn_oop/template.cpp
+++ learn_oop/template.cpp
@@ -6,6 +6,10 @@ template <class T>
 T add(T a,T b){
 	return a+b;
 }
+template <class T,class T2>
+T add(T a,T2 b){
+	return a+b;
+}
 /*template<class I,class O>
 O m(I a,I b,O){
 	return (O)(a-b);
@@ -18,7 +22,8 @@ int main(){
 	cout<<add(a1,b1)<<' '<<add(a2,b2)<<' '<<add(a3,b3)<<endl<<add(a4,b4)<<endl;
 	int (*p)(int,int);
 	p=add;
-	cout<<p(1,2);
+	cout<<p(1,2)<<endl;
+	cout<<add(a1,a2)<<"  "<<add(b2,b1)<<endl;
 //	cout<<m(2,1,double);
 	return 0;
 }
